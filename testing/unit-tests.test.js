5489b471750c1edfb9a7cd3c4aaef136a8bdd4b9
storyofwoe <julietmewett@icloud.com>
2026-06-12 16:18:47 +0100

    init unit testing

--- testing/unit-tests.test.js
+++ testing/unit-tests.test.js
@@ -1,10 +1,13 @@
+import cdk from "aws-cdk-lib/core";
+import { Template } from "aws-cdk-lib/assertions";
+const BakehouseStack = require("../lib/bakehouse-stack")
+
 // const cdk = require('aws-cdk-lib/core');
 // const { Template } = require('aws-cdk-lib/assertions');
 // const WillRobinsonBakehouseS3 = require('../lib/will-robinson-bakehouse-s3-stack');
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/will-robinson-bakehouse-s3-stack.js
-test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //   // WHEN
 //   const stack = new WillRobinsonBakehouseS3.WillRobinsonBakehouseS3Stack(app, 'MyTestStack');
@@ -14,4 +17,16 @@ test('SQS Queue Created', () => {
 //   template.hasResourceProperties('AWS::SQS::Queue', {
 //     VisibilityTimeout: 300
 //   });
-});
+
+
+test('creates S3 buckets', () => {
+    const app = new cdk.App();
+    const stack = new BakehouseStack(app, 'TestStack');
+
+    const template = Template.fromStack(stack);
+
+    const expected = 2;
+
+    template.resourceCountIs('AWS::S3::Bucket', expected)
+
+})
